76bcc3a7b093a66bd4280913f1df30f6add708b4
ahmedfesial <ahmedfesial150@gmail.com>
2025-10-01 17:59:23 +0300

    API

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // src/config.js
-export const API_BASE_URL = "https://muhjiblaravel-production.up.railway.app/api/";
+export const API_BASE_URL = "https://muhjiblaravel-production.up.railway.app/api";
 
